d8d91bae0d8cb2bab43e6083d8913088dd717083
lianjunqiang <lian.junqiang@iwhalecloud.com>
2023-06-17 16:09:12 +0800

    update chat api

--- views/src/api/chat.ts
+++ views/src/api/chat.ts
@@ -1,4 +1,3 @@
-import qs from 'qs'
 import { api } from './api'
 export const openChat = (params: any) => {
   return api({
@@ -32,23 +31,18 @@ function ajax(url) {
   })
 }
 
-export const chat = (message: any) => {
-//   return api({
-//     url: '/chat-docs/chatno',
-//     method: 'post',
-//     data: JSON.stringify(params),
-//   })
-  return api({
-    url: '/api/faas/serverless/gpt-api-gw/v1/chat/completions',
-    method: 'post',
-    data: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: message }],
-      max_tokens: 4000,
-      temperature: 1.0,
-    }),
-  })
-}
+// export const chat = (message: any) => {
+//   // return api({
+//   //   url: '/api/faas/serverless/gpt-api-gw/v1/chat/completions',
+//   //   method: 'post',
+//   //   data: JSON.stringify({
+//   //     model: 'gpt-3.5-turbo',
+//   //     messages: [{ role: 'user', content: message }],
+//   //     max_tokens: 4000,
+//   //     temperature: 1.0,
+//   //   }),
+//   // })
+// }
 export const chatOpenChat = (message: any) => {
   // return ajax(`http://127.0.0.1:5000/openai/${message}`)
   return ajax(`/test/openai/${message}`)
@@ -58,29 +52,48 @@ export const chatOpenChat = (message: any) => {
   // })
 }
 
-export const chatfile = (params: any) => {
+export const chat = (params: any) => {
   return api({
-    url: '/chatfile',
-    method: 'post',
-    data: qs.stringify(params),
-  })
-}
-
-export const getfilelist = () => {
-  return api({
-    url: '/chat-docs/list',
-    method: 'get',
-    params: {
-      knowledge_base_id: '123',
-    },
-
-  })
-}
-
-export const deletefile = (params: any) => {
-  return api({
-    url: '/chat-docs/delete',
+    url: '/chat',
     method: 'post',
     data: JSON.stringify(params),
   })
+}
+
+export const chatfile = (params: any) => {
+  return api({
+    url: '/local_doc_qa/local_doc_chat',
+    method: 'post',
+    data: JSON.stringify(params),
+  })
+}
+
+export const getfilelist = (knowledge_base_id: any) => {
+  return api({
+    url: '/local_doc_qa/list_files',
+    method: 'get',
+    params: { knowledge_base_id },
+
+  })
+}
+export const bing_search = (params: any) => {
+  return api({
+    url: '/local_doc_qa/bing_search_chat',
+    method: 'post',
+    data: JSON.stringify(params),
+
+  })
+}
+export const deletefile = (params: any) => {
+  return api({
+    url: '/local_doc_qa/delete_file',
+    method: 'post',
+    data: JSON.stringify(params),
+  })
+}
+export const web_url = () => {
+  return window.location.origin
+}
+export const setapi = () => {
+  return window.baseApi
 }
